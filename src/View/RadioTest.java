61cc2d331cc8804835d93757738c3fe2f5378f10
Daniel <dub22233@uvg.edu.gt>
2023-01-17 10:55:05 -0600

    Pruebas unitarias finalizadas

--- src/View/RadioTest.java
+++ src/View/RadioTest.java
@@ -4,6 +4,7 @@ import Model.Radio;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 public class RadioTest {
 
@@ -15,6 +16,24 @@ public class RadioTest {
             assertEquals(540,estacion);
 
         }
+
+
+        @Test
+    public void testBackward() {
+        Radio radio = new Radio("AM", 0, true);
+        radio.Backward();
+        int estacion = radio.getAMActualStation();
+        assertEquals(1610,estacion);
+
+    }
+
+    @Test
+    public void testOFF() {
+        Radio radio = new Radio("AM", 0, true);
+        radio.off();
+        assertFalse(radio.isOn());
+
+    }
 
 
 }
